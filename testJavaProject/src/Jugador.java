f0dfd68bbcc8a3f4bd24ea48f44365e584d09ae5
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-31 12:20:37 +0100

    Creadno contructores, getters and setters de la clase jugador

--- testJavaProject/src/Jugador.java
+++ testJavaProject/src/Jugador.java
@@ -1,8 +1,21 @@
+import java.sql.Date;
+
 public class Jugador {
     private String Nombre_camiseta;
     private Date Fecha_nacimiento;
     private String Pais_origen;
     private Posicion Posicion_jugador;
-    private int dorsal;
+    private int Dorsal;
     private Traspaso Traspaso_jugador;
+
+    public Jugador(String Nombre_camiseta, Date Fecha_nacimiento, String Pais_origen, Posicion Posicion_jugador,
+            int Dorsal, Traspaso Traspaso_jugador) {
+        this.Nombre_camiseta = Nombre_camiseta;
+        this.Fecha_nacimiento = Fecha_nacimiento;
+        this.Pais_origen = Pais_origen;
+        this.Posicion_jugador = Posicion_jugador;
+        this.Dorsal = Dorsal;
+        this.Traspaso_jugador = Traspaso_jugador;
+    }
+
 }
