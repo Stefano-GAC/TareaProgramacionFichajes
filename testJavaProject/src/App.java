9faae471c1bc72d45d0ae8c82d531b3c8ba44aff
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-31 13:06:14 +0100

    Creacion de objeto de la clase Posicion y Traspaso

--- testJavaProject/src/App.java
+++ testJavaProject/src/App.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+import Jugador.Posicion;
+import Jugador.Traspaso;
+
 public class App {
     public static void main(String[] args) {
         /** Creacion de objetos de la clase Presidente */
@@ -12,5 +15,19 @@ public class App {
         presidentes.add(presidente1);
         presidentes.add(presidente2);
         presidentes.add(presidente3);
+
+        /** Creacion de objeto de la clase Posicion y Traspaso */
+        Posicion posicionPortero = Posicion.Portero;
+        Posicion posicionDefensa = Posicion.Defensa;
+        Posicion posicionMedioCampista = Posicion.Medio_Campista;
+        Posicion posicionDelantero = Posicion.Delantero;
+        Posicion posicionAguatero = Posicion.Aguatero;
+
+        Traspaso traspasoSinSolicitar = Traspaso.SINSOLICITAR;
+        Traspaso traspasoSolicitado = Traspaso.SOLICITADO;
+        Traspaso traspasoAprobadoEntrenador = Traspaso.APROBADOENTRENADOR;
+        Traspaso traspasoAprobadoPresidente = Traspaso.APROBADOPRESIDENTE;
+        Traspaso traspasoRechazadoEntrenador = Traspaso.RECHAZADOENTRENADOR;
+        Traspaso traspasoRechazadoPresidente = Traspaso.RECHAZADOPRESIDENTE;
     }
 }
