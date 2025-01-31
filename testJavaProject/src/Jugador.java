dfc9b95095bfd01d1cee10ea4025551a55357c18
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-31 12:25:31 +0100

    modificando getter and setter

--- testJavaProject/src/Jugador.java
+++ testJavaProject/src/Jugador.java
@@ -18,4 +18,52 @@ public class Jugador {
         this.Traspaso_jugador = Traspaso_jugador;
     }
 
+    public String getNombre_camiseta() {
+        return Nombre_camiseta;
+    }
+
+    public void setNombre_camiseta(String Nombre_camiseta) {
+        this.Nombre_camiseta = Nombre_camiseta;
+    }
+
+    public Date getFecha_nacimiento() {
+        return Fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date Fecha_nacimiento) {
+        this.Fecha_nacimiento = Fecha_nacimiento;
+    }
+
+    public String getPais_origen() {
+        return Pais_origen;
+    }
+
+    public void setPais_origen(String Pais_origen) {
+        this.Pais_origen = Pais_origen;
+    }
+
+    public Posicion getPosicion_jugador() {
+        return Posicion_jugador;
+    }
+
+    public void setPosicion_jugador(Posicion Posicion_jugador) {
+        this.Posicion_jugador = Posicion_jugador;
+    }
+
+    public int getDorsal() {
+        return Dorsal;
+    }
+
+    public void setDorsal(int Dorsal) {
+        this.Dorsal = Dorsal;
+    }
+
+    public Traspaso getTraspaso_jugador() {
+        return Traspaso_jugador;
+    }
+
+    public void setTraspaso_jugador(Traspaso Traspaso_jugador) {
+        this.Traspaso_jugador = Traspaso_jugador;
+    }
+
 }
