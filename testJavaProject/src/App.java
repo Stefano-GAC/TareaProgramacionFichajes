3c9239a084745fa6f2488875abd75f8e259fc284
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-31 13:01:58 +0100

    Creando objetos y lista en main de la clase presidentes

--- testJavaProject/src/App.java
+++ testJavaProject/src/App.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        /** Creacion de objetos de la clase Presidente */
+        Presidente presidente1 = new Presidente("123456789A", "Carlos Martinez");
+        Presidente presidente2 = new Presidente("123456789B", "Camila Gonzalez");
+        Presidente presidente3 = new Presidente("123456789C", "Jose Rpdriguez");
+
+        /** Creacion de la lista de presidentes */
+        ArrayList<Presidente> presidentes = new ArrayList<>();
+        presidentes.add(presidente1);
+        presidentes.add(presidente2);
+        presidentes.add(presidente3);
     }
 }
