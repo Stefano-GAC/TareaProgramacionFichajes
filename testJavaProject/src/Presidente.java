8509d8995928f16bf4dff77b1d6ca3dfc1943df5
DESKTOP-L7H1FM4\Usuario <sac0024@alu.medac.es>
2025-01-31 12:55:15 +0100

    Añadiendo datos a la clase presidentes

---
+++ testJavaProject/src/Presidente.java
@@ -0,0 +1,11 @@
+
+public class Presidente {
+    public String Dni;
+    public String Casa;
+
+    public Presidente(String Dni, String Casa) {
+        this.Dni = Dni;
+        this.Casa = Casa;
+    }
+
+}
