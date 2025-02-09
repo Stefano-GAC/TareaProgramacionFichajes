eb7d8a515b2c216b5efe593bd430fc521485f8f7
CAMILAGP\Camila <cgp0037@alu.medac.es>
2025-02-09 22:44:44 +0100

    Documentación de Presidente, Entrenador y Jugador

--- testJavaProject/src/Entrenador/Entrenador.java
+++ testJavaProject/src/Entrenador/Entrenador.java
@@ -1,4 +1,5 @@
 package Entrenador;
+
 import Jugador.Jugador;
 import Equipo.Equipo;
 import Jugador.Traspaso;
@@ -9,36 +10,72 @@ public class Entrenador {
     private Traspaso Traspaso_jugador;
     private Equipo nombreEquipo;
 
+    /**
+     * Constructor de Entrnador
+     * 
+     * @param nombreEntrenador
+     * @param formacionPreferida
+     * @param nombreEquipo
+     */
     public Entrenador(String nombreEntrenador, Formacion formacionPreferida, Equipo nombreEquipo) {
         this.nombreEntrenador = nombreEntrenador;
         this.formacionPreferida = formacionPreferida;
         this.nombreEquipo = nombreEquipo;
     }
 
+    /**
+     * 
+     * @return El nombre del entrenador
+     */
     public String getNombreEntrenador() {
         return nombreEntrenador;
     }
 
+    /**
+     * Pone o modifica el nombre del entrenador
+     * 
+     * @param nombreEntrenador
+     */
     public void setNombreEntrenador(String nombreEntrenador) {
         this.nombreEntrenador = nombreEntrenador;
     }
 
+    /**
+     * 
+     * @return muestra la formacion elegida por un Entrenador
+     */
     public Formacion getFormacionPreferida() {
         return formacionPreferida;
     }
 
+    /**
+     * Pone o modifica la formacion de un entrenador para un equipo
+     * 
+     * @param formacionPreferida
+     */
     public void setFormacionPreferida(Formacion formacionPreferida) {
         this.formacionPreferida = formacionPreferida;
     }
 
-    public void entrenadorDecision(Jugador Nombre_camiseta){
+    /**
+     * 
+     * @param Nombre_camiseta Jugador que solicitará el cambio
+     * 
+     *                        Metodo que verifica el estado actual del traspaso de
+     *                        un jugador y lo compara con el estado "Solicitado",
+     *                        para mantenerlo o igualarlo.
+     */
+    public void entrenadorDecision(Jugador Nombre_camiseta) {
         if (Jugador.getTraspaso_jugador == Traspaso.SOLICITADO) {
             if (Jugador.getnombreEquipo == this.nombreEquipo) {
-                
+
             }
         }
     }
 
+    /**
+     * Metodo toString que permite mostrar los datos de la clase Entrenador
+     */
     @Override
     public String toString() {
         return "Entrenador [Nombre= " + nombreEntrenador + "Formación= " + formacionPreferida + "Equipo = "
--- testJavaProject/src/Jugador/Jugador.java
+++ testJavaProject/src/Jugador/Jugador.java
@@ -16,6 +16,16 @@ public class Jugador {
     private Traspaso Traspaso_jugador;
     private Equipo nombreEquipo;
 
+    /**
+     * Constructor del nuevo jugador
+     * 
+     * @param Nombre_camiseta
+     * @param Fecha_nacimiento
+     * @param Pais_origen
+     * @param Posicion_jugador
+     * @param Dorsal
+     * @param Traspaso_jugador
+     */
     public Jugador(String Nombre_camiseta, Date Fecha_nacimiento, String Pais_origen, Posicion Posicion_jugador,
             int Dorsal, Traspaso Traspaso_jugador) {
         this.Nombre_camiseta = Nombre_camiseta;
@@ -26,6 +36,10 @@ public class Jugador {
         this.Traspaso_jugador = Traspaso.SINSOLICITAR;
     }
 
+    /**
+     * 
+     * @return El nombre de la camiseta de un jugador
+     */
     public String getNombre_camiseta() {
         if (Nombre_camiseta == null) {
             System.out.println("El nombre ingresado no es valido");
@@ -33,34 +47,72 @@ public class Jugador {
         return Nombre_camiseta;
     }
 
+    /**
+     * Pone o modifica el nombre de la camiseta de un jugador
+     * 
+     * @param Nombre_camiseta
+     */
     public void setNombre_camiseta(String Nombre_camiseta) {
         this.Nombre_camiseta = Nombre_camiseta;
     }
 
+    /**
+     * 
+     * @return La fecha de nacimiento de un jugador
+     */
     public Date getFecha_nacimiento() {
         return Fecha_nacimiento;
     }
 
+    /**
+     * Pone o modifica la fecha de nacimiento ded un jugador
+     * 
+     * @param Fecha_nacimiento
+     */
     public void setFecha_nacimiento(Date Fecha_nacimiento) {
         this.Fecha_nacimiento = Fecha_nacimiento;
     }
 
+    /**
+     * 
+     * @return muestra el pais de origen de un jugador
+     */
     public String getPais_origen() {
         return Pais_origen;
     }
 
+    /**
+     * Pone o modifica el pais de origen de un jugador
+     * 
+     * @param Pais_origen
+     */
     public void setPais_origen(String Pais_origen) {
         this.Pais_origen = Pais_origen;
     }
 
+    /**
+     * 
+     * @return muestra la posicion de un jugador
+     */
     public Posicion getPosicion_jugador() {
         return Posicion_jugador;
     }
 
+    /**
+     * Pone o modifica la posicion de un jugador
+     * 
+     * @param Posicion_jugador
+     */
     public void setPosicion_jugador(Posicion Posicion_jugador) {
         this.Posicion_jugador = Posicion_jugador;
     }
 
+    /**
+     * 
+     * @return muestra el dorsal de la camisa de un jugador
+     *         Contiene una condicion para que el dorsal no pueda ser un numero
+     *         menor a 0
+     */
     public int getDorsal() {
         if (Dorsal < 0) {
             System.out.println("El dorsal es incorrecto");
@@ -68,26 +120,54 @@ public class Jugador {
         return Dorsal;
     }
 
+    /**
+     * Pone o modifica el dorsal de la camisa de un jugador
+     * 
+     * @param Dorsal
+     */
     public void setDorsal(int Dorsal) {
         this.Dorsal = Dorsal;
     }
 
+    /**
+     * 
+     * @return muestra el estado de traspaso de un jugador
+     */
     public Traspaso getTraspaso_jugador() {
         return Traspaso_jugador;
     }
 
+    /**
+     * Pone o modifia el estado de solicitud de traspaso de un jugador
+     * 
+     * @param Traspaso_jugador
+     */
     public void setTraspaso_jugador(Traspaso Traspaso_jugador) {
         this.Traspaso_jugador = Traspaso_jugador;
     }
 
+    /**
+     * 
+     * @return El nombre del equipo del jugador
+     */
     public Equipo getNombreEquipo() {
         return nombreEquipo;
     }
 
+    /**
+     * Pone o modifica el nombre del equipo de un jugador
+     * 
+     * @param nombreEquipo
+     */
     public void setNombreEquipo(Equipo nombreEquipo) {
         this.nombreEquipo = nombreEquipo;
     }
 
+    /**
+     * Metodo que verifica si el estado del traspaso esta en solicitar y si es así
+     * que se cambie a solicitado
+     * Y si es igual muestra que ya lo ha solicitado
+     */
     public void jugadorTraspaso() {
         if (Traspaso_jugador == Traspaso.SINSOLICITAR) {
             Traspaso_jugador = Traspaso.SOLICITADO;
@@ -98,6 +178,9 @@ public class Jugador {
 
     }
 
+    /**
+     * Metodo toString que permite mostrar los datos de la clase Jugador
+     */
     @Override
     public String toString() {
         return "Jugador [Nombre_camiseta= " + Nombre_camiseta + ", Fecha_nacimiento= " + Fecha_nacimiento
